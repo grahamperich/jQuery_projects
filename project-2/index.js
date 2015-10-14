8b9bdef9abc23205c5a3fe8473e674f937def629
Graham <graham.perich@gmail.com>
2015-10-13 20:21:23 -0700

    updated project 2 faq with jquery collapse functionality

---
+++ project-2/index.js
@@ -0,0 +1,14 @@
+$("#q1").click(function() {
+	$("#a1").slideToggle("fast");
+	$("#arrow1-down, #arrow1-up").toggleClass("collapse");
+})
+
+$("#q2").click(function() {
+	$("#a2").slideToggle("fast");
+	$("#arrow2-down, #arrow2-up").toggleClass("collapse");
+})
+
+$("#q3").click(function() {
+	$("#a3").slideToggle("fast");
+	$("#arrow3-down, #arrow3-up").toggleClass("collapse");
+})
