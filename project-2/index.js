7d9ed90f104ebd4081a583cbce69c3e9f272dd36
Graham <graham.perich@gmail.com>
2015-10-15 21:33:07 -0700

    updated project 3 to use persistent local storage

--- project-2/index.js
+++ project-2/index.js
@@ -1,14 +1,43 @@
-$("#q1").click(function() {
-	$("#a1").slideToggle("fast");
-	$("#arrow1-down, #arrow1-up").toggleClass("collapse");
+$(".question").click(function() {
+	$(this).next().slideToggle("fast");
+	$(this).children().toggleClass("collapse");
 })
 
-$("#q2").click(function() {
-	$("#a2").slideToggle("fast");
-	$("#arrow2-down, #arrow2-up").toggleClass("collapse");
+$("h1").mouseover(function() {
+	$("h1").animate({ fontSize: "31px" }, 1000);
+	$("h1").animate({ fontSize: "30px" }, 1000);
+		
+})
+	
+
+
+$("h1").click(function () {
+	console.log($(this));
+	$(this)
+	.animate({ 
+
+		opacity: 0.25, 
+		
+
+		}, 500, function() {
+		$(this).animate({
+			opacity: 1.0,
+			
+			})
+	});
 })
 
-$("#q3").click(function() {
-	$("#a3").slideToggle("fast");
-	$("#arrow3-down, #arrow3-up").toggleClass("collapse");
-})
+// $("#q1").click(function() {
+// 	$("#a1").slideToggle("fast");
+// 	$("#arrow1-down, #arrow1-up").toggleClass("collapse");
+// })
+
+// $("#q2").click(function() {
+// 	$("#a2").slideToggle("fast");
+// 	$("#arrow2-down, #arrow2-up").toggleClass("collapse");
+// })
+
+// $("#q3").click(function() {
+// 	$("#a3").slideToggle("fast");
+// 	$("#arrow3-down, #arrow3-up").toggleClass("collapse");
+// })
--- project-3/index.js
+++ project-3/index.js
@@ -1,3 +1,42 @@
 $(document).ready(function () {
-    // YOUR CODE HERE!
+
+	$("#list-items").html(localStorage.getItem("savedListItems"));
+
+
+    $(".add-items").submit(function(event){
+    	event.preventDefault();
+   
+
+    	var item = $("#todo-list-item").val(); 
+
+    	
+    	if (item) {
+    		$("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>");
+    		localStorage.setItem("savedListItems", $("#list-items").html());
+    		$("#todo-list-item").val(""); 
+    	}
+
+    }); //add item to list
+
+	$(document).on("change", ".checkbox", function() {
+
+		if($(this).attr("checked")) 
+			
+			{$(this).removeAttr("checked");} 
+
+		else {$(this).attr("checked", "checked");}
+
+		$(this).parent().toggleClass("completed");
+		localStorage.setItem("savedListItems", $("#list-items").html());
+
+
+	}); //add strikethrough to finished tasks
+
+
+	$(document).on("click", ".remove", function(){
+
+		$(this).parent().remove()
+		localStorage.setItem("savedListItems", $("#list-items").html());
+	}); //remove list item
+
 });
